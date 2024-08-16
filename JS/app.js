cd8de12c83e0242d2137d783c7812186e5c85af7
nmorrone <n.morrone96@gmail.com>
2024-08-16 18:09:38 +0200

    end

--- JS/app.js
+++ JS/app.js
@@ -43,5 +43,31 @@ const animals = [
 
 const mammiferi = []
 
-//verifico funzionamento
-console.log(animals)
+
+/*apro un ciclo for per ciclare gli elementi pushare nell'array vuoto
+tutti gli animali appartenenti alla classe mammiferi dell'array Animals */
+
+
+//apro ciclo for con lunghezza dell'array Animals
+for (let i = 0; i < animals.length; i++) {
+
+    //dichiaro la costante che acquisisce l'oggetto
+    const currentAnimal = animals[i]
+
+    //verifico funzionamento acquisizione
+    console.log(currentAnimal)
+
+
+    //dichiaro la condizione che mi interessa
+    if (currentAnimal.classe === 'mammiferi') {
+
+        //se vera pusho
+        mammiferi.push(currentAnimal)
+    }
+
+}
+
+/*controllo che nell'array mammiferi vengano 
+inseriti solo gli oggetti appartenente alla categoria */
+
+console.log(mammiferi)
