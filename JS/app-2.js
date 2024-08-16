bf39df7619d0a0c8926e32404edd2feb59e9137d
nmorrone <n.morrone96@gmail.com>
2024-08-16 18:47:51 +0200

    for-and-if-else

--- JS/app-2.js
+++ JS/app-2.js
@@ -43,3 +43,37 @@ const people = [
         età: 54,
     },
 ]
+
+//verifico funzionamento
+console.log(people)
+
+//creo un nuove Array per il controllo
+const validation = []
+
+//apro ciclo for
+for (let i = 0; i < people.length; i++) {
+
+    let currentPerson = people[i]
+
+    //verifico funzionamento
+    console.log(currentPerson)
+
+    //apro condizione di maggiore età
+    if (currentPerson.età >= 18) {
+
+        currentPerson = currentPerson.nome + ' ' + currentPerson.cognome + ' Congratulazioni puoi guidare!'
+
+        validation.push(currentPerson)
+
+
+    }
+
+    //condizione di minore età
+    else {
+        currentPerson = currentPerson.nome + ' ' + currentPerson.cognome + ' Mi dispiace non puoi guidare!'
+        validation.push(currentPerson)
+    }
+
+}
+
+console.log(validation)
