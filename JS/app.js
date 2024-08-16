b9aacf3c14b4c8c41e4618615ed9739294634d38
nmorrone <n.morrone96@gmail.com>
2024-08-16 17:43:02 +0200

    objects-4

--- JS/app.js
+++ JS/app.js
@@ -2,3 +2,39 @@ console.log('JS-Snacks-Objects')
 
 /*crea un array di oggetti che rappresentano degli animali.
 Ogni animale ha un nome, una famiglia e una classe */
+
+
+//dichiaro l'array che conterrà gli oggetti
+
+const animals = [
+
+    //inserisco primo oggetto
+    {
+        nome: 'leona',
+        famiglia: 'felidi',
+        classe: 'mammiferi'
+    }
+
+    //inserisco secondo oggetto
+
+    {
+        nome: 'cane',
+        famiglia: 'canidi',
+        classe 'mammiferi',
+    }
+
+
+    //inserisco terzo oggetto
+    {
+        nome: 'gallina',
+        famiglia: 'fasianidi',
+        classe: 'uccelli',
+    }
+
+    //inserisco quarto oggetto
+    {
+        nome: 'coccodrillo',
+        famiglia: 'crocodilidi',
+        classe: 'rettili',
+    }
+]
