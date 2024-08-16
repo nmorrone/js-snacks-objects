4a41c356efcc3c519444bd4447c4a83806fbf64f
nmorrone <n.morrone96@gmail.com>
2024-08-16 17:54:57 +0200

    animals-array-empty

--- JS/app.js
+++ JS/app.js
@@ -1,47 +1,8 @@
-console.log('JS-Snacks-Objects')
+console.log('JS Snacks Variabili')
 
-/*crea un array di oggetti che rappresentano degli animali.
-Ogni animale ha un nome, una famiglia e una classe */
-
-
-//dichiaro l'array che conterrà gli oggetti
+/* crea un array che contiene una lista di animali
+con varie proprietà che li categorizzano */
 
 const animals = [
 
-    //inserisco primo oggetto
-    {
-        nome: 'leona',
-        famiglia: 'felidi',
-        classe: 'mammiferi'
-    },
-
-    //inserisco secondo oggetto
-
-    {
-        nome: 'cane',
-        famiglia: 'canidi',
-        classe 'mammiferi',
-    },
-
-
-    //inserisco terzo oggetto
-    {
-        nome: 'gallina',
-        famiglia: 'fasianidi',
-        classe: 'uccelli',
-    },
-
-    //inserisco quarto oggetto
-    {
-        nome: 'coccodrillo',
-        famiglia: 'crocodilidi',
-        classe: 'rettili',
-    }
 ]
-
-//verifico funzionamento
-console.log(animals)
-
-//creo un nuovo array dei mammiferi
-
-const mammiferi = []
