b51eeebd6a1dc7f5672a68a9e344ec76ccd393a3
nmorrone <n.morrone96@gmail.com>
2024-08-16 18:17:03 +0200

    people-array

--- JS/app-2.js
+++ JS/app-2.js
@@ -1,1 +1,45 @@
 console.log('JS Snacks 2 - Patente')
+
+
+/*crea un array che contiene un elenco di persone con 
+(nome, cognome, età), successivamente crea un altro array
+che contenga una frase con nome e cognome della persone e,
+in base all'età della stessa, se può o non può guidare */
+
+
+//creo Array persone objects
+
+const people = [
+
+    {
+        nome: 'Tommaso',
+        cognome: 'Rossi',
+        età: 24,
+    },
+
+    {
+        nome: 'Sara',
+        cognome: 'Verdi',
+        età: 23,
+    },
+
+
+
+    {
+        nome: 'Lucia',
+        cognome: 'Garibaldi',
+        età: 15,
+    },
+
+    {
+        nome: 'Marco',
+        cognome: 'Celentano',
+        età: 17,
+    },
+
+    {
+        nome: 'Giuseppe',
+        cognome: 'Rossi',
+        età: 54,
+    },
+]
