cd9f8c6490dc2d8b9af9c4189dc60ac4e4ae7086
nmorrone <n.morrone96@gmail.com>
2024-08-16 18:00:13 +0200

    animals-objects

--- JS/app.js
+++ JS/app.js
@@ -3,6 +3,45 @@ console.log('JS Snacks Variabili')
 /* crea un array che contiene una lista di animali
 con varie proprietà che li categorizzano */
 
+
+//crea un array che contiene una lista di "oggetti"
 const animals = [
 
+    {
+        nome: 'leone',
+        famiglia: 'felidi',
+        classe: 'mammiferi',
+    },
+
+    {
+        nome: 'tigre',
+        famiglia: 'felidi',
+        classe: 'mammiferi',
+    },
+
+    {
+        nome: 'cane',
+        famiglia: 'canidi',
+        classe: 'mammiferi',
+    },
+
+    {
+        nome: 'gallina',
+        famiglia: 'fasianidi',
+        classe: 'uccelli',
+    },
+
+    {
+        nome: 'coccodrillo',
+        famiglia: 'crocodilidi',
+        classe: 'rettili',
+    },
+
 ]
+
+//creo un altro array vuoto che conterrà una singola categoria
+
+const mammiferi = []
+
+//verifico funzionamento
+console.log(animals)
